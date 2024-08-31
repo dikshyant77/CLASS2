6f5f7ce68d135966018a5c07a5790898b38dcdc9
dikshyant77 <rayamajhidikshyant@gmail.com>
2024-08-31 21:07:15 +0545

    Add files via upload

---
+++ Class 2/main.py
@@ -0,0 +1,32 @@
+#printing a sentence 
+print("welcome to the world of programming")
+
+#print a number 
+print(4)
+
+#next line after print
+print("hello python \n")
+
+#pass multiple argument
+print("hello ", 5)
+
+#end argument of print statement
+print("welcome to ",end="*")
+print()
+
+#creating a variable
+name="Dikshyant"
+print("my name ",name)
+
+#creating a variable age
+age=12
+print("my age is",age )
+
+#creating a variable hobby and asking user input
+hobby= input("what is your hobby \n")
+print("wow nice hobby ", hobby)
+
+import keyword 
+print("python keywoard are \n")
+print(keyword.kwlist)
+print(len(keyword.kwlist))
